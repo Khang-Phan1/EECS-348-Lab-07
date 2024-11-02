cf963c4db64a3218a370a9874154bab8cb52f058
Khang Phan <khangphan0515@gmail.com>
2024-11-02 01:16:51 -0500

    Complete Template

--- matrix-template-1.cpp
+++ matrix-template-1.cpp
@@ -11,22 +11,68 @@ private:
 
 public:
   // 1. Read values from stdin into a matrix
-  void readFromStdin();
+  void readFromStdin() {
+    for (int i=0; i<SIZE; i++) {
+      for (int j=0; j<SIZE; j++) {
+        cin >> data[i][j];
+      }
+    }
+  }
 
   // 2. Display a matrix
-  void display() const;
+  void display() const {
+    for (int i=0; i<SIZE; i++) {
+      for (int j=0; j<SIZE; j++) {
+        cout << data[i][j] << "\t";
+      }
+      cout << endl;
+    }
+  }
 
   // 3. Add two matrices (operator overloading for +)
-  Matrix operator+(const Matrix& other) const;
+  Matrix operator+(const Matrix& other) const {
+    Matrix sum;
+    for (int i=0; i<SIZE; i++) {
+      for (int j=0; j<SIZE; j++) {
+        sum.data[i][j] = data[i][j] + other.data[i][j];
+      }
+    }
+    return sum;
+  }
 
   // 4. Multiply two matrices (operator overloading for *)
-  Matrix operator*(const Matrix& other) const;
+  Matrix operator*(const Matrix& other) const {
+    Matrix product;
+    for (int i=0; i<SIZE; i++) {
+      for (int j=0; j<SIZE; j++) {
+        product.data[i][j] = 0;
+
+        for (int k=0; k<SIZE; k++) {
+          product.data[i][j] += data[i][k] * other.data[k][j];
+        }
+      }
+    }
+    return product;
+  }
 
   // 5. Compute the sum of matrix diagonal elements
-  int sumOfDiagonals() const;
+  int sumOfDiagonals() const {
+    int sum;
+    for (int i=0; i<SIZE; i++) {
+      sum += data[i][i];
+    }
+    return sum;
+  }
 
   // 6. Swap matrix rows
-  void swapRows(int row1, int row2);
+  void swapRows(int row1, int row2) {
+    int temp[SIZE];
+    for (int i=0; i<SIZE; i++) {
+      temp[i] = data[row1][i];
+      data[row1][i] = data[row2][i];
+      data[row2][i] = temp[i];
+    }
+  }
 };
 
 int main() {
